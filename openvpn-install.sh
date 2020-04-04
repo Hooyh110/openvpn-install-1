b04065cda1039fb79bd1394c266df31ab2d2a2d8
Hooyh110 <yuhao_hu@live.cn>
2020-04-04 19:09:41 +0800

    .sh修改

--- openvpn-install.sh
+++ openvpn-install.sh
@@ -1224,7 +1224,7 @@ function removeOpenVPN () {
 function manageMenu () {
 	clear
 	echo "Welcome to OpenVPN-install!"
-	echo "The git repository is available at: https://github.com/angristan/openvpn-install"
+	echo "The git repository is available at: https://github.com/Hooyh110/openvpn-install"
 	echo ""
 	echo "It looks like OpenVPN is already installed."
 	echo ""
